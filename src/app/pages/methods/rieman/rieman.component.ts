407f71c72213fbfe21aa04b0404553c6230a2861
unknown <carlossolorock@gmail.com>
2018-11-25 15:03:09 -0500

    correcion del metodo de rieman, se modifica la forma en como se hacen los calculos

--- src/app/pages/methods/rieman/rieman.component.ts
+++ src/app/pages/methods/rieman/rieman.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { parse,simplify } from 'mathjs';
+import { parse, simplify } from 'mathjs';
 
 @Component({
   selector: 'app-rieman',
@@ -13,25 +13,27 @@ export class RiemanComponent implements OnInit {
   @Input() public valmin: number = 0;
   @Input() public valmax: number = 0;
   @Input() public valitera: number = 0;
-  @Input() public valeval: string= "";
+  @Input() public valeval: string = "";
   displayedColumns: string[] = ['id', 'xi', 'fxi', 'ai']
 
   dataSource = [];
-  constructor() { 
-    
+  constructor() {
+
   }
   ngOnInit() {
     this.mathMethods();
   }
-  
+
   mathMethods() {
     let current: number = this.valmin;
-    let max: number  = this.valmax;
-    let itera: number  = this.valitera;
+    let max: number = this.valmax;
+    let itera: number = this.valitera;
+    let dx: number = (max - current) / itera;
 
     //console.log("this.valuemin", current);
     //console.log("this.valuemax", max);
     //console.log("this.valueitera", itera);
+    //console.log("this.dx", dx);
 
     let id = 0;
     let iterations = [];
@@ -41,19 +43,19 @@ export class RiemanComponent implements OnInit {
 
       iterations.push({
         id,
-        xi : current.toFixed(20),
-        fxi : evl.toFixed(20),
-        ai: (evl * itera).toFixed(20)
+        xi: current.toFixed(20),
+        fxi: evl.toFixed(20),
+        ai: (evl * dx).toFixed(20)
       })
-      current += itera;
+      current += dx;
     }
     this.dataSource = iterations;
-    //console.log("iterations" , iterations);
+    console.log("iterations", iterations);
   }
 
   /* Evaluar en la formula sin integrar */
-  evaluar( value : number ){
-    
+  evaluar(value: number) {
+
     //Variable que se utiliza como temporal mientras se recorre el metodo.
     /*let evl = "";
     //Ciclo para recorrer el string o valor de la funcion para wolfram
@@ -68,8 +70,8 @@ export class RiemanComponent implements OnInit {
     }
     let pru = simplify(evl);
     let val : number = Number.parseFloat(pru.toString());*/
-    let node = parse(this.valeval) 
-    let eval2 = node.eval({x: value})   
+    let node = parse(this.valeval)
+    let eval2 = node.eval({ x: value })
     //2(-2.8)+3(-2.8)^2
     //let val = ((simplify(evl).toin / (10 ** 4))*(10 ** 4));
     //funcion simplify de mathjs para evaluar la funcion en string.
